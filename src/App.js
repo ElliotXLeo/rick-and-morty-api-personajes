f2fb959a6ae098984f35da22f0dc636abf4ff916
ElliotXLeo <elliotgaramendi@gmail.com>
2021-02-17 22:45:02 -0500

    Estilos a components CSS y SASS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Characters from './components/Characters';
 import Header from './components/Header';
+import './main.sass';
+import './components/Header.css';
 
 const App = () => {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,97 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
+const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
     filename: "js/bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     publicPath: ""
   },
-  mode: "production",
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
         resolve: {
-          extensions: [".js", ".jsx"]
-        }
+          extensions: ['.js', '.jsx'],
+        },
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
-          "css-loader"
-        ]
+            'style-loader', 
+            'css-loader'
+        ],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
+        use:  [{
+            loader: 'file-loader',
             options: {
-              name: "images/[contenthash].[ext]"
+              name: 'images/[contenthash].[ext]'
             }
-          }
-        ]
+        }]
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+            "style-loader",
+            "css-loader",
+            "sass-loader"
         ],
         resolve: {
-          extensions: [".sass", ".scss"]
+            extensions: ['.sass', '.scss']
         }
       },
-      {
-        test: /\text$/,
-        use: "raw-loader"
+      { 
+        test: /\.txt$/, 
+        use: 'raw-loader' 
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        use: "asset/resource"
+        type: 'asset/resource',
       },
       {
         test: /\.(csv|tsv)$/i,
-        use: "csv-loader"
+        use: ['csv-loader'],
       },
       {
         test: /\.xml$/i,
-        use: "xml-loader"
-      },
-      {
+        use: ['xml-loader'],
+      },{
         test: /\.toml$/i,
-        type: "json",
+        type: 'json',
         parser: {
-          parse: toml.parse
-        }
+          parse: toml.parse,
+        },
       },
       {
         test: /\.yaml$/i,
-        type: "json",
+        type: 'json',
         parser: {
-          parse: yaml.parse
-        }
+          parse: yaml.parse,
+        },
       },
       {
         test: /\.json5$/i,
-        type: "json",
+        type: 'json',
         parser: {
-          parse: json5.parse
-        }
+          parse: json5.parse,
+        },
       }
-    ]
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
-  ]
-}
+      template: './public/index.html',
+    }),
+  ],
+};
